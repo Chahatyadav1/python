f1be0da30284512b25219e8ef0d1882fe1c74870
Chahat Yadav <chahatyadav855@gmail.com>
2026-06-29 00:34:39 +0530

    Day-1

--- Day-1.py
+++ Day-1.py
@@ -3,9 +3,11 @@ content = file.read()
 print(content)
 
 import os
+import shutil
 import os.path
 
 os.mkdir("test")
 # var=os.path.exists("test")
 # print(var)
 os.rmdir("test")
+# shutil.rmtree('')  // use to delete complete folder remove tree
